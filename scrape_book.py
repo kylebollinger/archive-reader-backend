83862aaddb29aed2baa39e9a1ad31282e818b4d5
Kyle Bollinger <kbollinger32@gmail.com>
2023-08-10 23:29:02 -0700

    [save point] Scrape missing books

---
+++ scrape_book.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import requests, csv, re, string, secrets, time, random
+import sys, os
+
+from models import Book, BookVolume, BookChapter, create_new_session
+from scraper.utils import getHTMLdocument, generate_slug_id
+
+
+session = create_new_session()
+
+# book = session.query(Book).filter_by(id=274).first()
+# book = session.query(Book).filter_by(id=280).first() # has wikipedia link in middle of text
+book = session.query(Book).filter_by(id=296).first()
+
+
+def create_base_volume(book):
+    # Create a new BookVolume instance
+    base_volume = BookVolume(
+        title="Contents",
+        book_id=book.id,
+        sequence=1,
+        data={},
+        import_data={
+            "vol_id": generate_slug_id(),
+            "book_url": book.import_data.get('web_url'),
+            "vol_title": "Contents",
+            "vol_sequence": 1,
+            "vol_url": book.import_data.get('web_url')
+        }
+    )
+    session.add(base_volume)
+    session.commit()
+
+    return base_volume
+
+
+def scrape_volumes(book):
+    book_url = book.import_data.get('web_url')
+    html_doc, encoding = getHTMLdocument(book_url)
+    soup = BeautifulSoup(html_doc, "html.parser")
+    volumes = session.query(BookVolume).filter_by(book_id=book.id).all()
+
+    # Delete all center tags
+    for center_tag in soup.find_all('center'):
+        center_tag.decompose()
+
+    if len(soup.find_all('a')) > 0 and not volumes:
+        """ Time to create a base volume and extract chapters """
+        chap_sequence = 0
+        # base_volume = create_base_volume(book)
+
+        # Then Find all the link tags
+        for link_tag in soup.find_all('a'):
+            """ After removing all the <center> tags with links, the remaining <a> tags are the ones we want for the chapters
+
+                chapter_titles ==> link_tag.text
+                chapter_urls   ==> link_tag.get('href')
+            """
+            if "http" in link_tag.get('href'):
+                """ Not internal link, cant be a chapter """
+                continue
+
+            chap_sequence += 1
+
+            if link_tag.find_next_sibling('a') or link_tag.find_previous_sibling('a'):
+                """ This should be link in a list of chapters """
+                print(link_tag.get('href'))
+
+                # Create a new BookChapter instance
+
+
+
+scrape_volumes(book)
