a4be5e64c03ddfd99fa4b21d0db189be12564eb3
Kyle Bollinger <kbollinger32@gmail.com>
2023-08-14 19:24:18 -0700

    Add new example functions to run.py

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from scraper.scrapers import scrape_book, bulk_scrape_books
-from db.cleaners import eliminate_body_headers
+from db.cleaners import (
+    process_body_center_tags,
+    bulk_process_body_center_tags,
+    update_book_sequence,
+    bulk_update_book_sequences
+)
 
 """
     Example 1: Scrape a single book
@@ -8,4 +13,12 @@ from db.cleaners import eliminate_body_headers
     Example 2: Bulk Scrape a list of books
     ===> bulk_scrape_batch = [1416, 1430, 1431, 1432, 1434, 1435, 1436, 1439, 1441, 1443]
     ===> bulk_scrape_books(bulk_scrape_batch)
+
+    Example 3: Cleaning chapter body <center> tags
+    ===> [one] process_body_center_tags(chapter_id)
+    ===> [all] bulk_process_body_center_tags()
+
+    Example 4: Update book_sequence for book chapters
+    ===> [one] update_book_sequence(book_id)
+    ===> [all] bulk_update_book_sequences()
 """
