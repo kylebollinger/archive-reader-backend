7bd6462e657f93f10586262e2444216f8d10bdc9
Kyle Bollinger <kbollinger32@gmail.com>
2023-08-14 17:42:38 -0700

    Add processor to eliminate center tags (Header and Footers on pages)

--- scraper/processors.py
+++ scraper/processors.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 
 from db.models import Book, BookVolume, BookChapter, create_new_session
 from scraper.utils import getHTMLdocument, generate_slug_id
@@ -81,3 +82,45 @@ def create_base_volume(book):
     session.close()
     return base_volume
 
+
+def clean_center_tags(chapter_body):
+    soup = BeautifulSoup(chapter_body, "html.parser")
+
+    """ Knock out Footer """
+    if soup.select('hr + center'):
+        center_tag = soup.select_one('hr + center')
+        if len(center_tag.find_next_siblings()) == 0:
+            center_tag.find_previous_sibling('hr').decompose()
+            center_tag.decompose()
+
+    """ Search and destroy header/footer fallback """
+    for center_tag in soup.find_all('center'):
+        if center_tag.find_previous_siblings('hr') or center_tag.find_next_siblings('hr'):
+            if center_tag.find_previous_sibling('hr'):
+                center_tag.find_previous_sibling('hr').decompose()
+            if center_tag.find_next_siblings('hr'):
+                center_tag.find_next_sibling('hr').decompose()
+            center_tag.decompose()
+        center_tag.decompose()
+
+    """ Look for sub headers """
+    if soup.find_all('p', align="CENTER"):
+        # NOTE I think they do an <h3> tag as well
+        for centered_p in soup.find_all('p', align="CENTER"):
+            if centered_p.find_previous_siblings('hr') or centered_p.find_next_siblings('hr'):
+                if centered_p.find_previous_sibling('hr'):
+                    centered_p.find_previous_sibling('hr').decompose()
+                if centered_p.find_next_siblings('hr'):
+                    centered_p.find_next_sibling('hr').decompose()
+                centered_p.decompose()
+
+    """ If the first link is available for TOC, try to delete previous"""
+    if soup.find(attrs={"name": re.compile("page")}):
+        first_p = soup.find(attrs={"name": re.compile("page")})
+        header_trash_tags = first_p.parent.find_previous_siblings()
+        for tag in header_trash_tags:
+            tag.decompose()
+
+
+    body_tag = soup.find('body')
+    return body_tag
