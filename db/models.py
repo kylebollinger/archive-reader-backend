af4b6fb9caa8f431c098f8213c8c2aea84ef7b74
Kyle Bollinger <kbollinger32@gmail.com>
2023-09-26 10:53:29 -0700

    Switch to new postgres db
    - Migrate from mysql to postgres using psycopg2
    - Drop User model requirements (not needed)

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, JSON, DateTime, ForeignKey
+import psycopg2
 from sqlalchemy import Enum
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,33 +7,14 @@ import requests, os
 import pymysql
 
 from bs4 import BeautifulSoup
+from sqlalchemy import (
+    create_engine,
+    Column, Integer, String,
+    Text, JSON, DateTime,
+    ForeignKey
+)
 
-pymysql.install_as_MySQLdb()
 Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    email = Column(String(255))
-    username = Column(String(255))
-    password = Column(String(255))
-    photo = Column(String(255))
-    website = Column(String(255))
-    job_title = Column(String(255))
-    role = Column(Enum('admin', 'user', name='user_role'), default='user')
-    course_created = Column(Integer, default=0)
-    course_purchased = Column(Integer, default=0)
-    status = Column(Enum('active', 'inactive'), default='active')
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
-    deleted_at = Column(DateTime)
-    bio = Column(Text)
-    remember_token = Column(Text)
-
-    books = relationship("Book", back_populates="user")
 
 
 class Book(Base):
@@ -53,13 +34,10 @@ class Book(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     deleted_at = Column(DateTime)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     category_id = Column(Integer, ForeignKey('book_categories.id'))
     state = Column(String(255), nullable=False, default='initialized')
 
-    user = relationship('User', back_populates='books')
     category = relationship('BookCategory', back_populates='books')
-    user = relationship("User", back_populates="books")
     volumes = relationship("Volume", back_populates="book")
 
     def __repr__(self):
@@ -136,7 +114,7 @@ BookVolume.book = relationship('Book', back_populates='volumes')
 
 
 def create_new_session():
-    engine = create_engine('mysql://xero:xeropoint@localhost/xeropoint')
+    engine = create_engine('postgresql+psycopg2://xero:xeropoint@localhost/xeropoint')
 
     Base.metadata.create_all(engine)
 
